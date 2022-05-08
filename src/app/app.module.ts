8d84b1f2553f4d9623d7c446b4562501da67b334
Markos21 <markossebhat21@gmail.com>
2022-05-08 07:43:43 +0300

    4 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,15 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { NavbarComponent } from '../navbar/navbar.component';
+import { ProductListComponent } from '../product-list/product-list.component';
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, HelloComponent, NavbarComponent],
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    NavbarComponent,
+    ProductListComponent,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
